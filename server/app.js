fba73d76e81b8a1b86a97363ad223d86189c016a
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-05-23 01:43:12 -0400

    user app for prod container, api logs whether in prod or dev

--- server/app.js
+++ server/app.js
@@ -5,6 +5,13 @@ const morgan = require('koa-morgan')
 
 const app = koa()
 
+if (process.env.NODE_ENV === 'prod') {
+  console.log('Running in production mode')
+} else if (process.env.NODE_ENV === 'dev') {
+  console.log('Running in development mode')
+}
+
+
 app.use(morgan.middleware('dev'))
 
 app.use(function *(next) {
